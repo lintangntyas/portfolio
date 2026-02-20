2f50e83c2b2709693c08a9502033404e6ff1b00d
lintangntyas <81798062+lintangntyas@users.noreply.github.com>
2026-02-20 14:42:51 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-slate-50 text-slate-800">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
