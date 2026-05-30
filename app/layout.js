7ef7960c20ee68ae1a23e30ba19b3110c22bc09a
lintangntyas <81798062+lintangntyas@users.noreply.github.com>
2026-05-30 13:23:16 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Portfolio - Lintang Cahyaningtyas",
@@ -8,7 +9,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+
+        {/* VERCEL ANALYTICS */}
+        <Analytics />
+      </body>
     </html>
   );
 }
